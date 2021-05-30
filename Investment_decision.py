8394ccd381c1b3b0fdf2e31809ac3bb0d6d58789
Bagheera <kathrada.ridwan@icloud.com>
2021-05-30 09:26:01 +0200

    Final commit of first project

--- Investment_decision.py
+++ Investment_decision.py
@@ -1,10 +1,12 @@
 
-import numpy_financial as np  
+import numpy_financial as npf
+import numpy as np
+
 #Intro
-print("Welcome to this program which will help you in your investment decision ! ""you'll give me informations on your investment  and i'll compute the ")
+print("Welcome to this program which will help you in your investment decision ! \nYou'll give me informations on your investment  and i'll compute: \n\n-The Net Plus Value, \n-Internal Return rate \n-Excess Return")
 
 # asking the duration of the study and setting it in a list
-duration = int(input("duration of the study ? "))
+duration = input("\nWhat's the duration of our study ? ")
 time_line = []
 final_time_line = []
 def timeline(para):
@@ -13,61 +15,84 @@ def timeline(para):
         for n in t:
            final_time_line.append(n)
 
-timeline(duration)
+timeline(int(duration))
 
 # asking for the cost of investment 
 
-input_investment = int(input("cost of investment ? " ))
+input_investment = int(input("\nWhat will it cost ? " ))
 
 # asking  if FCF are fixed or variable number
 
-yorn = input("does the FCF stay the same for the entire study ? Y or N ")
+yorn = input("\nDoes the FCF will be the same for the entire study ? Y or N ")
 
 # asking for the value(s) of FCF 
 
 cfflst = []
 def ask_CFF(para2):
     if para2 == "Y":
-        cff0 = int(input("how much will be the FCF ? "))
+        cff0 = int(input("\nHow much will be the FCF ? "))
         cfflst.append(cff0)
         return cfflst
     else:
-      print("what are the FCF for the {} years of the study ?(put a space between each FCF) ".format(duration))
-      inputcff = int(input("")).split()
+      print("\nWhat are the FCF for the {} years of our study ?\n(put a space between each FCF) ".format(duration))
+      inputcff = input("").split(" ")
       for inputc in inputcff:
-          cfflst.append(int(inputc))
+          cfflst.append(float(inputc))
       return cfflst
         
 fcf1 = ask_CFF(yorn)
 
 # asking for the discount rate
 
-discountr = int(input("What discount rate do you want to use ? (number in % please) "))
+discountr = int(input("\nWhat discount rate are we using ? "))
 
 # computing the returns on investment and commenting 
 
 result_lst= []
 def return_investment_computing(years,fixed,cost,fcf2,rate):
     npv = 0
+    cumcff = []
     irr = 0
-    excess_return = 0
+    #excess_return = 0
     #computation with only one FCF
     if fixed == "Y":
         #Npv computation
-        npv += -(cost) 
+        npv += -(cost)
         for year in years[1:]:
             npv += (fcf2[0]/((1+rate)**(year)))
+            cumcff.append(fcf2[0])
         #irr computation
-        irr += np.irr([(-cost),fcf2])
+        k = np.array(-cost)
+        c = np.array(cumcff)
+        z = np.concatenate((k,c),axis = None)
+        irr += npf.irr(z)
+    #computation with different FCF
+    else:
+        #Npv computation
+        npv += -(cost)
+        for year in years[1:]:
+            npv += (fcf2[year-1]/((1+rate)**(year)))
+            cumcff.append(fcf2[year-1])
+         #irr computation
+        k = np.array(-cost)
+        c = np.array(cumcff)
+        z = np.concatenate((k,c),axis = None)
+        irr += npf.irr(z)
 
-        #excess return computation
+        
     result_lst.append(npv)
-    result_lst.append(irr)
+    result_lst.append(irr*100)
+    result_lst.append((irr-rate)*100)
     return result_lst
 
 return_investment_computing(final_time_line,yorn,input_investment,cfflst,(discountr/100))
 
-print(result_lst)
+print(("\n\n\nFor an investment of {} On a {} period, \nThe IRR for this project is {}% \nAnd with a Discount rate of {}% \nthe NPV will be {}\nAnd the Excess return {}%.").format(input_investment,duration,round(result_lst[1]),discountr,round(result_lst[0]),round(result_lst[2])))
+
+if result_lst[0] >= 0:
+    print("\nYou should pursue this project it's a good investment to make money! ")
+else:
+    print("\nYou shouldn't invest in this project, you'll loose money !\n\n\n\n\n")
 
 
 
